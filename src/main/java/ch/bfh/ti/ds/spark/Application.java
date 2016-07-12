3448af9a44add41d77d00e209de4e166a4604726
vgj1 <vgj1@bfh.ch>
2016-07-12 17:23:02 +0200

    update

--- src/main/java/ch/bfh/ti/ds/spark/Application.java
+++ src/main/java/ch/bfh/ti/ds/spark/Application.java
@@ -1,10 +1,23 @@
 package ch.bfh.ti.ds.spark;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class Application {
+	private SparkConf sparkConf;
+	private JavaSparkContext sparkContext;
+	
+	public Application() {
+		sparkConf = new SparkConf().setAppName("ch.bfh.ti.ds.task01");
+		sparkContext = new JavaSparkContext(sparkConf);
+	}
+
+	public JavaSparkContext getSparkContext() {
+		return sparkContext;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
